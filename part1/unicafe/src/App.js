056cac37f6e29ec666754e0e05b442820332e608
Milind Chauhan <milindchauhan95@gmail.com>
2021-03-26 09:26:24 +0530

    exercise 1.11 complete unicafe

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+const Button = ({ handleClick,text }) => {
+  return(
+    <>
+    <button onClick={handleClick}>
+      {text}
+    </button>
+    </>
+  )
+}
+
+const Statistic = ({ text,value }) => {
+  return(
+  <>
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+  </>
+  )
+}
+const Statistics = ({ good,neutral,bad }) => {
+  if (good === 0 && neutral === 0 && bad === 0) {
+    return (
+      <>
+      <h1>statistics</h1>
+      <p>No feedback given</p>
+      </>
+    )
+  }
+
+  return(
+    <>
+      <h1>statistics</h1>
+      <table>
+        <tbody>
+          <Statistic text='good' value={good} />
+          <Statistic text='neutral' value={neutral} />
+          <Statistic text='bad' value={bad} />
+          <Statistic text='all' value={good + neutral + bad} />
+          <Statistic text='average' value={(good - bad)/(good+neutral+bad)} />
+          <Statistic text='positive' value={(good/(good+neutral+bad))*100} />
+        </tbody>
+      </table>
+    </>
+  )
+}
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleGood = () => setGood(good+1)
+  const handleNeutral = () => setNeutral(neutral+1)
+  const handleBad = () => setBad(bad+1)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>give feedback</h1>
+      <div>
+        <Button handleClick={handleGood} text='good' />
+        <Button handleClick={handleNeutral} text='neutral' />
+        <Button handleClick={handleBad} text='bad' />
+      </div>
+
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
-  );
+  )
 }
 
 export default App;
